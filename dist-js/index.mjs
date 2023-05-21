56ce9dae001cf8f978a0f0c23bdfe001435aa26d
Lucas Fernandes Nogueira <lucas@tauri.studio>
2023-05-21 14:09:27 +0000

    chore: add remaining mirrors (#382)
    Co-authored-by: Fabian-Lars <fabianlars@fabianlars.de>
    
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/5038084282
    
    Co-authored-by: lucasfernog <lucasfernog@users.noreply.github.com>

---
+++ dist-js/index.mjs
@@ -0,0 +1,37 @@
+import { invoke } from '@tauri-apps/api/tauri';
+
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+/**
+ * Exits immediately with the given `exitCode`.
+ * @example
+ * ```typescript
+ * import { exit } from '@tauri-apps/plugin-process';
+ * await exit(1);
+ * ```
+ *
+ * @param code The exit code to use.
+ * @returns A promise indicating the success or failure of the operation.
+ *
+ * @since 1.0.0
+ */
+async function exit(code = 0) {
+    return invoke("plugin:process|exit", { code });
+}
+/**
+ * Exits the current instance of the app then relaunches it.
+ * @example
+ * ```typescript
+ * import { relaunch } from '@tauri-apps/plugin-process';
+ * await relaunch();
+ * ```
+ *
+ * @returns A promise indicating the success or failure of the operation.
+ *
+ * @since 1.0.0
+ */
+async function relaunch() {
+    return invoke("plugin:process|restart");
+}
+
+export { exit, relaunch };
+//# sourceMappingURL=index.mjs.map
